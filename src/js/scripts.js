8a418376c73ffaa4f94a24085da632df7ce05ec4
akhiljacob1 <akhiljacob98@gmail.com>
2024-04-03 12:42:41 +0100

    Threes code start

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,36 @@
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
-const renderer = new THREE.WebGLRendere()
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(800, 800)
 
-renderer.setSize(window.innerWidth, window.InnerHeight)
+document.body.appendChild(renderer.domElement)
 
-document.body.appendChild(rendere.domElement)
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+const orbit = new OrbitControls(camera, renderer.domElement)
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+camera.position.set(0, 2, 5)
+orbit.update()
+
+const boxGeometry = new THREE.BoxGeometry()
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00})
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+scene.add(box)
+
+function animate() {
+  box.rotation.x += 0.01
+  box.rotation.y += 0.01
+  renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
