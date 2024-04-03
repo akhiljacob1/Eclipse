0e3c57465416de42ef57b2e723bb0212a92091e3
akhiljacob1 <akhiljacob98@gmail.com>
2024-04-03 11:58:00 +0100

    Initialize

---
+++ src/js/scripts.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three'
+
+const renderer = new THREE.WebGLRendere()
+
+renderer.setSize(window.innerWidth, window.InnerHeight)
+
+document.body.appendChild(rendere.domElement)
